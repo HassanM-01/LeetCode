c916cf0f9e87f67676fbacd22b875e9a81ec9d45
Hassan Momin <hmomin2001@gmail.com>
2023-09-08 09:16:34 -0500

    added documentation for CWMW solution

--- Medium/ContainerWithMostWater.py
+++ Medium/ContainerWithMostWater.py
@@ -2,14 +2,14 @@ from typing import List
 
 
 def maxArea(self, height: List[int]) -> int:
-    l, r = 0, len(height) - 1
-    res = 0
+    l, r = 0, len(height) - 1  # initialize l & r pointers
+    res = 0  # initialize result var
 
-    while l < r:
-        res = max(res, min(height[l], height[r]) * (r - l))
-        if height[l] < height[r]:
+    while l < r:  # loop through list
+        res = max(res, min(height[l], height[r]) * (r - l))  # calculate if current container is more than result
+        if height[l] < height[r]:  # move left pointer right if it's less than height[r]
             l += 1
-        elif height[r] <= height[l]:
+        elif height[r] <= height[l]:  # move right pointer left if it's less than height[l]
             r -= 1
 
     return res
