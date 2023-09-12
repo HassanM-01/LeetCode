8e7ad72983c1f4d2f564c6313a6a5f8994e145c0
Hassan Momin <hmomin2001@gmail.com>
2023-09-12 12:50:46 -0500

    Added documentation for LongestRepeatingCharReplacement.py

--- Medium/LongestRepeatingCharReplacement.py
+++ Medium/LongestRepeatingCharReplacement.py
@@ -1,14 +1,14 @@
 def characterReplacement(self, s: str, k: int) -> int:
-    count = {}
-    res = 0
+    count = {}  # count occurrence of characters
+    res = 0  # length of longest substring/window
 
-    l = 0
-    for r in range(len(s)):
-        count[s[r]] = 1 + count.get(s[r], 0)
+    l = 0  # initialize left pointer
+    for r in range(len(s)):  # loop over chars in string
+        count[s[r]] = 1 + count.get(s[r], 0)  # increment occurrences of each char
 
-        while (r - 1 + 1) - max(count.values()) > k:
-            count[s[r]] -= 1
-            l += 1
+        while (r - 1 + 1) - max(count.values()) > k:  # check to see if total replaced chars are =< k
+            count[s[r]] -= 1  # decrement occurrences of char at left pointer
+            l += 1  # increment left pointer
 
-        res = max(res, r -1 + 1)
+        res = max(res, r - 1 + 1)  # calculate max window/substring size
     return res
