6698312bd2e29a525cf66580e023e49cabd0b476
Hassan Momin <hmomin2001@gmail.com>
2023-09-13 13:50:32 -0500

    Added 2nd solution + documentation for changes

--- Medium/LongestRepeatingCharReplacement.py
+++ Medium/LongestRepeatingCharReplacement.py
@@ -6,9 +6,25 @@ def characterReplacement(self, s: str, k: int) -> int:
     for r in range(len(s)):  # loop over chars in string
         count[s[r]] = 1 + count.get(s[r], 0)  # increment occurrences of each char
 
-        while (r - 1 + 1) - max(count.values()) > k:  # check to see if total replaced chars are =< k
+        while (r - l + 1) - max(count.values()) > k:  # check to see if total replaced chars are =< k
             count[s[r]] -= 1  # decrement occurrences of char at left pointer
             l += 1  # increment left pointer
 
-        res = max(res, r - 1 + 1)  # calculate max window/substring size
+        res = max(res, r - l + 1)  # calculate max window/substring size
     return res
+
+
+def characterReplacements(self, s: str, k: int) -> int:
+    count = {}
+
+    l = 0
+    maxf = 0  # count frequency of most occurring char
+    for r in range(len(s)):
+        count[s[r]] = 1 + count.get(s[r], 0)
+        maxf = max(maxf, count[s[r]])  # calculate max frequency
+
+        if (r - l + 1) - maxf > k:
+            count[s[l]] -= 1
+            l += 1
+
+    return (r - l + 1)
