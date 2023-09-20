18c83a39e2fd60d9ba562248beeedd5db075d541
Hassan Momin <hmomin2001@gmail.com>
2023-09-20 13:09:46 -0500

    Added LCS documentation

--- Medium/LongestConsecutiveSequence.py
+++ Medium/LongestConsecutiveSequence.py
@@ -2,14 +2,14 @@ from typing import List
 
 
 def longestConsecutive(self, nums: List[int]) -> int:
-    numSet = set(nums)
-    longest = 0
+    numSet = set(nums)  # create set from list of nums
+    longest = 0  # initialize variable to hold length of longest sequence
 
-    for n in nums:
-        if (n - 1) not in numSet:
-            length = 0
-            while (n + length) in numSet:
-                length += 1
-            longest = max(length, longest)
+    for n in nums:  # loop over list
+        if (n - 1) not in numSet:  # check if number is beginning of a sequence
+            length = 0  # intialize length variable
+            while (n + length) in numSet:  # continue to loop while sequence continues
+                length += 1  # increment length variable
+            longest = max(length, longest)  # calculate longest substring
 
     return longest
