5b8a4eb7cb7b3f37c82b428d2db24dab06583218
Hassan Momin <hmomin2001@gmail.com>
2023-09-22 13:24:12 -0500

    Added evaluatRPM documentation

--- Medium/EvaluateRPN.py
+++ Medium/EvaluateRPN.py
@@ -2,18 +2,18 @@ from typing import List
 
 
 def evalRPN(self, tokens: List[str]) -> int:
-    stack = []
-    for c in tokens:
-        if c == '+':
-            stack.append(stack.pop() + stack.pop())
+    stack = []  # create stack
+    for c in tokens:  # loop through input
+        if c == '+':  # check for operand
+            stack.append(stack.pop() + stack.pop())  # pop last two elements and append result
         elif c == '-':
-            a, b = stack.pop(), stack.pop()
-            stack.append(b-a)
+            a, b = stack.pop(), stack.pop()  # assign popped elements to variables in order to switch ordering
+            stack.append(b - a)  # append result
         elif c == '*':
-            stack.append(stack.pop() * stack.pop())
+            stack.append(stack.pop() * stack.pop())  # pop last two elements and append result
         elif c == '/':
-            a, b = stack.pop(), stack.pop()
-            stack.append(int(b / a))
+            a, b = stack.pop(), stack.pop()  # assign popped elements to variables in order to switch ordering
+            stack.append(int(b / a))  # cast to int to round to integer
         else:
             stack.append(int(c))
-        return stack[0]
+        return stack[0]  # return top of stack
