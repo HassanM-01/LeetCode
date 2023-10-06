2f5a20d45490c8f5416cfa4d43650c2130fb76e9
Hassan Momin <hmomin2001@gmail.com>
2023-10-06 11:02:40 -0500

    added ReverseString.py  problem documentation

--- Easy/ReverseString.py
+++ Easy/ReverseString.py
@@ -3,9 +3,9 @@ from typing import List
 
 def reverseString(self, s: List[str]) -> None:
 
-    l = 0
-    r = len(s) - 1
-    while l < r:
-        s[l], s[r] = s[r], s[l]
-        l += 1
-        r -= 1
+    l = 0  # initialize left pointer to beginning of string
+    r = len(s) - 1  # initialize right pointer to end of string
+    while l < r:  # loop through string
+        s[l], s[r] = s[r], s[l]  # switch char at left pointer to char at right pointer & vice versa
+        l += 1  # increment left pointer to move towards end of string
+        r -= 1  # decrement right pointer to move towards beginning of string
