b1b9666543a1f3ed83bd1373e76c4e61a91410d2
Hassan Momin <hmomin2001@gmail.com>
2023-10-16 14:34:21 -0500

    added ProductOfArrayExceptSelf.py documentation

--- Medium/ProductOfArrayExceptSelf.py
+++ Medium/ProductOfArrayExceptSelf.py
@@ -2,14 +2,14 @@ from typing import List
 
 
 def productExceptSelf(self, nums: List[int]) -> List[int]:
-    res = [1] * len(nums)
+    res = [1] * len(nums)  # Create a list 'res' of the same length as the input 'nums'.
 
     prefix = 1
-    for i in range(len(nums)):
-        res[i] = prefix
-        prefix *= nums[i]
+    for i in range(len(nums)):  # Loop through the 'nums' list from left to right.
+        res[i] = prefix  # Set the current element of 'res' to the 'prefix' value.
+        prefix *= nums[i]  # Update the 'prefix' by multiplying it with the current element of 'nums'.
     postfix = 1
-    for i in range(len(nums) -1, -1, -1):
-        res[i] *= postfix
-        postfix *= nums[i]
+    for i in range(len(nums) -1, -1, -1):  # Loop through the 'nums' list from right to left.
+        res[i] *= postfix   # Update the current element in 'res' by multiplying it with 'postfix'.
+        postfix *= nums[i]  # Update 'postfix' by multiplying it with the current element of 'nums'.
     return res
