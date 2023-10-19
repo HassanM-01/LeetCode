1b8acd794c62c7e57bb83f93bc482565cf683dd5
Hassan Momin <hmomin2001@gmail.com>
2023-10-19 11:09:56 -0500

    added hackerrank question seen on technical interview

---
+++ Medium/ArraySubsets.py
@@ -0,0 +1,24 @@
+arr = [3, 7, 5, 6, 2,]
+
+
+def subsetA(arr):
+    res = []
+    arr.sort(reverse=True)
+    print(arr)
+    minimum = len(arr) // 2
+    sum = 0
+    sum_b = 0
+
+    for i in range(minimum):
+        sum += arr[i]
+    for y in range(minimum + 1, len(arr) - 1):
+        sum_b += arr[y]
+    print(sum)
+    print(sum_b)
+    if sum >= sum_b:
+        res = arr[:minimum]
+    print(res)
+    return res
+
+
+subsetA(arr)
