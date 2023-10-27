e5ce51a6b785b2db9c9f80888a15d5e17977ae99
Hassan Momin <hmomin2001@gmail.com>
2023-10-27 11:53:45 -0500

    added ReverseLinkedList.py line by line docs

--- Easy/ReverseLinkedList.py
+++ Easy/ReverseLinkedList.py
@@ -3,12 +3,12 @@ from typing import Optional
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
+        prev, curr = None, head   # Initialize two variables, prev and curr, to None and the input head
 
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
+        while curr:   # Start a loop that continues as long as curr is not None (as long as there are more nodes to process)
+            nxt = curr.next   # Create a new variable called nxt to store the next node in the original list
+            curr.next = prev  # Update the next pointer of the current node (curr) to point to the previous node (prev)
+            prev = curr   # Move prev and curr one step forward in the list
             curr = nxt
         return prev
 
